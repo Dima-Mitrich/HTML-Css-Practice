be901c11335fd7d37e3627d71679173a0020674d
Dima Mitrich <34646268+Dima-Mitrich@users.noreply.github.com>
2020-11-10 21:34:08 +0200

    l7

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,37 @@
-const ShowModal = () => {
-    const scrollY = document.documentElement.style.getPropertyValue('--scroll-y');
+var modal = document.getElementById("window");
+var OpenLink = document.getElementById("login");
+var CloseBtn = document.getElementById("close1");
+var InputEmail = document.getElementById("ModalEmail"); 
+
+OpenLink.onclick = function() {
+    modal.style.display = 'block';
+    InputEmail.focus();
     const body = document.body;
     body.style.height = '100vh';
     body.style.overflowY = 'hidden';
-};
+}
 
-const CloseModal = () => {
+CloseBtn.onclick = function() {
+    modal.style.display = 'none';
     const body = document.body;
     const scrollY = body.style.top;
     body.style.height = '';
     body.style.overflowY = '';
-};
+}
+
+/*function ShowModal() {
+    var modal = document.getElementById("window");
+    const body = document.body;
+    body.style.height = '100vh';
+    body.style.overflowY = 'hidden';
+    modal.style.display = 'block';
+}
+
+function CloseModal() {
+    const body = document.body;
+    var modal = document.getElementById("window");
+    const scrollY = body.style.top;
+    body.style.height = '';
+    body.style.overflowY = '';
+    modal.style.display = 'none';
+}*/
